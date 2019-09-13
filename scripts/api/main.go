c187cc1bc413c9269731da1caa4fe10a46acf14d
mhutchinson <mhutchinson@gmail.com>
2019-09-13 12:30:50 +0100

    Extra scraping to details submember count

--- scripts/api/main.go
+++ scripts/api/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"go/types"
+	"strings"
 	"unicode"
 
 	"golang.org/x/tools/go/packages"
@@ -16,10 +18,49 @@ func main() {
 		panic(err)
 	}
 	for _, p := range ps {
-		for _, n := range p.Types.Scope().Names() {
+		s := p.Types.Scope()
+		for _, n := range s.Names() {
 			if unicode.IsUpper([]rune(n)[0]) {
-				fmt.Printf("%v.%v\n", p, n)
+				var sb strings.Builder
+				sb.WriteString(fmt.Sprintf("%v.%v: ", p, n))
+				switch t := s.Lookup(n).(type) {
+				case *types.Func:
+					var sig *types.Signature = t.Type().(*types.Signature)
+					sb.WriteString(fmt.Sprintf("\tfunction (%v params, %v returns)", sig.Params().Len(), sig.Results().Len()))
+				case *types.TypeName:
+					numMethods, numFields := scrapeType(t)
+					sb.WriteString(fmt.Sprintf("\tclass (%v methods, %v fields)", numMethods, numFields))
+				default:
+					sb.WriteString(fmt.Sprintf("\t%T", t))
+				}
+				fmt.Printf("%v\n", sb.String())
 			}
 		}
 	}
+}
+
+// Returns the number of methods and fields in the type
+func scrapeType(t *types.TypeName) (int, int) {
+	var named *types.Named = t.Type().(*types.Named)
+
+	var expFields int
+	switch u := named.Underlying().(type) {
+	case *types.Struct:
+		for i := 0; i < u.NumFields(); i++ {
+			if u.Field(i).Exported() {
+				expFields++
+			}
+		}
+	default:
+		// No fields to count I guess
+	}
+
+	var expMethods int
+	for i := 0; i < named.NumMethods(); i++ {
+		if named.Method(i).Exported() {
+			expMethods++
+		}
+	}
+
+	return expMethods, expFields
 }
