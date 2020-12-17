436a0966e5b5390fa9dee9cd27bc09136c9e8f62
Martin Hutchinson <mhutchinson@gmail.com>
2020-12-17 12:42:07 +0000

    Very evil routing to bind localhost to public interface
    This now fails at a different hurdle 'failed to connect to all addresses' and I'm becoming less convinced this is worth the effort

--- experimental/batchmap/runner/start.sh
+++ experimental/batchmap/runner/start.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+NETIP=$(ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
+socat TCP4-LISTEN:8099,bind=${NETIP},reuseaddr,fork TCP4:localhost:8099 &
+
 python -m apache_beam.runners.portability.local_job_service_main --port=8099
